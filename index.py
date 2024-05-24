fd2921fcb783a810c75997b197944c629b035f07
diego <diego.maustem@gmail.com>
2024-05-24 11:29:29 -0300

    :recycle: refactor: Acrescentando validação campo preço

--- index.py
+++ index.py
@@ -1,13 +1,23 @@
 invoice = []
 insertProduct = 's'
 amount = 0 
+valid_price = False
 
 while insertProduct != 'n':
     product = input('Insert the product: ')
-    price = float(input('Enter the price of the product: '))
+
+    while valid_price == False:
+        price = input('Enter the price of the product: ')
+        try:
+            price = float(price)
+            valid_price = True
+        except: 
+            print('Format of price invalid. Use only numbers with .')
+
     invoice.append([product, price])
 
     amount += price
+    valid_price = False
     insertProduct = input('Do you want to add any more products? S or N ').lower()
 
 for i in invoice:
