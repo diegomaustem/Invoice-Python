55da39268999a61c358cdc7aa95075769fddb84d
diego <diego.maustem@gmail.com>
2024-05-24 11:04:31 -0300

    :sparkles: feat: Exercicio de curso Python

---
+++ index.py
@@ -0,0 +1,17 @@
+# Exercício Fatura ::: 
+invoice = []
+insertProduct = 's'
+amount = 0 
+
+while insertProduct != 'n':
+    product = input('Insert the product: ')
+    price = float(input('Enter the price of the product: '))
+    invoice.append([product, price])
+
+    amount += price
+    insertProduct = input('Do you want to add any more products? S or N ').lower()
+
+for i in invoice:
+    print(i[0], '-','$',i[1])
+
+print('Amount payable:', '$',amount)
